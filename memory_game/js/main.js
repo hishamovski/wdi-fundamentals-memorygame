92d093746010d5ab2cc02d7714184a383cedc4a2
hisham boualam <hisham.boualam@gmail.com>
2019-04-08 15:05:58 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+
+var cardOne = "queen";
+var cardtwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardtwo);
+console.log("User flipped " + cardFour);
